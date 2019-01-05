133d17c1a7a7fe6ddc0acbac64077b479f04e8f0
natariolucas <natarrc5@gmail.com>
2019-01-05 16:06:14 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+var Twitter = require('twitter');
+var ontime = require('ontime');
+
+var client = new Twitter({
+ // LUCAS consumer_key: 'vXUKKXfpl3i7KelwkX5dTfgac',
+ // consumer_secret: '7h7e6Qrcps6tlyhuXkJbsQ4gqdw9LLlzEcZaQ26jiGqSoVSD8K',
+ // access_token_key: '1891919095-lLWvCEWKfEEhuvwcxNQlZ0zZpn29a0fwsouUCtk',
+ // access_token_secret: '5HqyYPYgnPxLHf6lp6cttEzKH5jwJRxlKkTDebYwdB6B2'
+  consumer_key: 'EJeDRXQy1crTvTblYHWDBZAb5',
+  consumer_secret: 'g4xcxd91rVfJkMunNLZrSlaiw4F5QeG1wn90DftVvKwlhSVGIR',
+  access_token_key: '260895654-2B5xGJO5KIwrdo8lj3ldtYjvF9kcsFlr6zb3Av25',
+  access_token_secret: 'Vkut6POrdzFZLKfIbSfQuqFLFToaTQBtlnBBw9P1O4VRv'
+});
+
+ const idTweetDecision = '1075178276870602752';
+ const idMyTwitterAcc = '1891919095'; //Lucas
+ const idMyTwitterAcc2 = '260895654' //Uber
+ var arrHorarios = [];
+ var arrIds = [];
+ // ------------------------------------STREAM
+  var stream = client.stream('statuses/filter', {
+      follow: idMyTwitterAcc2
+  });
+
+  stream.on('data', function(event) {
+    //Se muestra la fecha actual y se muestra la fecha + 24hs
+    var d = new Date(); 
+    d.setDate(d.getUTCDate());
+    console.log('Fecha: ' + d);
+ 
+    d.setDate(d.getDate() + 1); //Suma (1 dia)
+    //getMonth +1 es porque cuenta Enero = 0, Dic = 11
+    var formatedDate = (d.getMonth() + 1).toString() + '-' + (d.getDate()).toString() + 'T' + (d.getHours()).toString() + ':' + (d.getMinutes()).toString() + ':' + (d.getSeconds()).toString();
+    
+    console.log('Fecha +24: ' + d);
+    console.log('Fecha formateada + 24: ' + formatedDate);
+
+   arrHorarios.push(formatedDate); //Se agrega la fecha +24hs al array que dispara el ontime
+   arrIds[formatedDate] = event.id_str; //Se agrega el horario y se asocia a un ID de tweet
+
+   //   -----------------------------------RENOVAR ONTIME 
+     var OT = ontime({
+       cycle: arrHorarios,
+       utc: true
+     }, function (ot) {
+      var dOT = new Date();
+      dOT.setDate(dOT.getUTCDate());
+
+      console.log('Horario de ejecución (UTC): ' + dOT)
+     
+       ot.done()
+       return
+     })
+  
+  });
+
+  stream.on('error', function(error) {
+    console.log(error);
+    });
+
