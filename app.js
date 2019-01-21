9d096e4e1bc10141e568032514a496ae30f019b0
Lucas <natarrc5@gmail.com>
2019-01-21 18:14:43 -0300

    re activar con cuenta Lucas

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ var Twitter = require('twitter');
 var ontime = require('ontime');
 
 var client = new Twitter({
- //LUCASconsumer_key: 'vXUKKXfpl3i7KelwkX5dTfgac',
- //consumer_secret: '7h7e6Qrcps6tlyhuXkJbsQ4gqdw9LLlzEcZaQ26jiGqSoVSD8K',
- //access_token_key: '1891919095-lLWvCEWKfEEhuvwcxNQlZ0zZpn29a0fwsouUCtk',
- //access_token_secret: '5HqyYPYgnPxLHf6lp6cttEzKH5jwJRxlKkTDebYwdB6B2'
- consumer_key: 'EJeDRXQy1crTvTblYHWDBZAb5',
- consumer_secret: 'g4xcxd91rVfJkMunNLZrSlaiw4F5QeG1wn90DftVvKwlhSVGIR',
- access_token_key: '260895654-2B5xGJO5KIwrdo8lj3ldtYjvF9kcsFlr6zb3Av25',
- access_token_secret: 'Vkut6POrdzFZLKfIbSfQuqFLFToaTQBtlnBBw9P1O4VRv'
+ consumer_key: 'vXUKKXfpl3i7KelwkX5dTfgac',
+ consumer_secret: '7h7e6Qrcps6tlyhuXkJbsQ4gqdw9LLlzEcZaQ26jiGqSoVSD8K',
+ access_token_key: '1891919095-lLWvCEWKfEEhuvwcxNQlZ0zZpn29a0fwsouUCtk',
+ access_token_secret: '5HqyYPYgnPxLHf6lp6cttEzKH5jwJRxlKkTDebYwdB6B2'
+ //UBERconsumer_key: 'EJeDRXQy1crTvTblYHWDBZAb5',
+ //consumer_secret: 'g4xcxd91rVfJkMunNLZrSlaiw4F5QeG1wn90DftVvKwlhSVGIR',
+ //access_token_key: '260895654-2B5xGJO5KIwrdo8lj3ldtYjvF9kcsFlr6zb3Av25',
+ //access_token_secret: 'Vkut6POrdzFZLKfIbSfQuqFLFToaTQBtlnBBw9P1O4VRv'
 });
 
  //const idTweetDecision = '1075178276870602752';
@@ -109,7 +109,7 @@ function formatDateToday (d) {
 
  // ------------------------------------STREAM
   var stream = client.stream('statuses/filter', {
-      follow: idMyTwitterAcc2
+      follow: idMyTwitterAcc
   });
 
   stream.on('data', function(tweet) { 
